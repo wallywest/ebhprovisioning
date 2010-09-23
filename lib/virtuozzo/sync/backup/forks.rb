f4e660d7b1727ad231b45372937fabb2809f1c50
root <root@server.scarletspeedster.com>
2010-09-23 17:56:00 -0500

    learning some git

--- lib/virtuozzo/sync/backup/forks.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'init'
-require_relative 'emsync'
-10.times do
-    Sync::run(@config)
-end
--- lib/virtuozzo/sync/backup/sqltests.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'init'
-require_relative 'ippool'
-require 'active_record'
-@ips=Vdspool.where("veid='20167'").select("ips")
-@pool=Virtuozzo::IPool::new
-p @pool.cclass
-@pool.cclass={"hello"=>"hello"}
-p @pool.cclass
-Virtuozzo::IPool::giveips(@ips.first.ips)
